0ded45a3a1f5e9e3a657448342616355db7c2f12
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-02-19 17:53:13 +0600

    site fully responsive

---
+++ dummy-text.js
@@ -0,0 +1,5 @@
+let aboutTextContentContainer = document.getElementById("about-text-content");
+let contactContentContainer = document.getq("contact-content");
+
+contactContentContainer.innerText = ``;
+aboutTextContentContainer.innerText = ``;
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 const toggleNavbar = () => {
   let navBar = document.getElementById("nav-links");
   navBar.classList.toggle("hideNavbar");
 };
+
